201edf7834f75b4c99f62d1f6626669728fece7b
Julie Tran <kimtran767@gmail.com>
2022-03-31 11:27:19 -0700

    ignored files

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const routes = require('./routes');
 //create and return server
 const server = http.createServer(routes.requestHandler)
 
-
-
-
 //listen start a process keep it running for incoming
 //request
 server.listen(3000);
+
+
+
 
 
 
